a9cd1a10a876f129c17115067998c9c09266b1c1
AlexanderDP08 <57006179+AlexanderDP08@users.noreply.github.com>
2022-04-01 21:59:43 +0700

    logo loading

---
+++ app/src/main/java/com/example/project_manpro/Logo_Loading.kt
@@ -0,0 +1,11 @@
+package com.example.project_manpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Logo_Loading : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logo_loading)
+    }
+}
